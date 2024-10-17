edd1765c9b32ca9cfa1c1c300fa912ed72b42af4
Naif Otibi <nn.n@outlook.cl>
2024-10-18 01:25:20 +0300

    adding font and make things great

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,12 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+const nunitoRegular = localFont({
+  src: "./fonts/nunito-regular.woff",
+  variable: "--font-nunito-regular",
   weight: "100 900",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${nunitoRegular.variable}`}>
         {children}
       </body>
     </html>
