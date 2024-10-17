f951890d57d4ed31b13d63be9b6c93f3d3d109e3
Naif Otibi <nn.n@outlook.cl>
2024-10-18 01:00:29 +0300

    complete the structure and adding box-shadow

--- src/app/components/Blog.tsx
+++ src/app/components/Blog.tsx
@@ -14,8 +14,12 @@ export default function Blog(){
                 <div className="card__content__up__badge">Learning</div>
                 <div className="card__content__up__date">Published 21 Dec 2023</div>
                 <div className="card__content__up__heading" role="heading" aria-level={1}>HTML & CSS Foundation</div>
+                <div className="card__content__up__paragraph" role="paragraph">These languages are the backbone of every website, defining structure, content, and presentation.</div>
             </div>
-            <div className="card__content__down"></div>
+            <div className="card__content__down">
+                <div className="card__content__down__image"></div>
+                <div className="card__content__down__name" role="heading" aria-level={4}>Greg Hooper</div>
+            </div>
         </div>
     </div>
 }
