7d41c1bfb71b0b2ea20c171337f64f28d2646b2e
Naif Otibi <nn.n@outlook.cl>
2024-10-18 00:56:23 +0300

    craft the structuree

--- src/app/components/Blog.tsx
+++ src/app/components/Blog.tsx
@@ -9,5 +9,13 @@ export default function Blog(){
         <div className="card__image">
             <Image src={main_image} />
         </div>
+        <div className="card__content">
+            <div className="card__content__up">
+                <div className="card__content__up__badge">Learning</div>
+                <div className="card__content__up__date">Published 21 Dec 2023</div>
+                <div className="card__content__up__heading" role="heading" aria-level={1}>HTML & CSS Foundation</div>
+            </div>
+            <div className="card__content__down"></div>
+        </div>
     </div>
 }
